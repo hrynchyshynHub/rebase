b419cf72579fd95024bff5f7c104ce64b83cd476
ihr@edilion.com <hrynchyshynKI33>
2019-03-14 16:10:58 +0200

    second-feature2
    feature2-ammend-add-sout

--- src/Feature2.java
+++ src/Feature2.java
@@ -4,5 +4,6 @@
 public class Feature2 {
     public static void main(String[] args) {
         System.out.println("Init feature 2");
+        System.out.println("Second commit - feature2");
     }
 }
