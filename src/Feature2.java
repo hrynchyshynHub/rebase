c5e9660e5497a6f4c735df9838d820121dca8e81
ihr@edilion.com <hrynchyshynKI33>
2019-03-14 16:21:09 +0200

    third-commit-edited

--- src/Feature2.java
+++ src/Feature2.java
@@ -6,5 +6,8 @@ public class Feature2 {
         System.out.println("Init feature 2");
         System.out.println("Second commit - feature2");
         System.out.println("Second commit - before amend -> after");
+
+        System.out.println("Third commit");
+        System.out.println("Third commit after ammend");
     }
 }
